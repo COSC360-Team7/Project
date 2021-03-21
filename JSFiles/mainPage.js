aba40205126f7d0e5a13d963ebf79a6b2675cb14
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-03-20 19:45:24 -0600

    bug fix

--- JSFiles/mainPage.js
+++ JSFiles/mainPage.js
@@ -1,12 +1,12 @@
 $("#login-btn").on("click",(function(e){
   var div = $('<div id="login"><p id="close" >x</p><h1>Registration</h1><form method="get" action="#" id="sampleForm"><input type="text" placeholder="UserName"/><br><input type="text" name="email" id="email" placeholder="Email" /><br><input type="password" name="pass" id="pass" placeholder="Password" /><br><button id="login-submit" type="submit" >Login</button></form></div>');
   //idk why but it didn't work when done on seperate lines so i just did it in one :)
-  $(this).append(div);
-  $("login").fadeIn(1000);
+  $(this).after(div);
+  $("#login").fadeIn(1000);
 }));
-$("#login").on("mouseleave",(function(e){
+$("#close").on("click",(function(e){
   $("#login").fadeOut(1000);
-  $("#login").remove();
+  $("login").remove();
   console.log("this method is called");
   // e.preventDefault();
 
