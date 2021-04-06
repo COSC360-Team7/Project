d250bda67c3a5c212e76500c7cf7b68a844b0d0f
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-04-06 12:23:33 -0600

    sign up page done

--- JSFiles/mainPage.js
+++ JSFiles/mainPage.js
@@ -3,11 +3,14 @@ $("#login-btn").on("click",(function(e){
   //idk why but it didn't work when done on seperate lines so i just did it in one :)
   $(this).after(div);
   $("#login").fadeIn(1000);
+  //just adds the eventlistener to close the login div/form
+  $("#close").on("click",(function(e){
+    $("#login").fadeOut(1000);
+    $("#login").remove();
+    console.log("this method is called");
+    // e.preventDefault();
+  
+  }));
 }));
-$("#close").on("click",(function(e){
-  $("#login").fadeOut(1000);
-  $("login").remove();
-  console.log("this method is called");
-  // e.preventDefault();
 
-}));
+//
