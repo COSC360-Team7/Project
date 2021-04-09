23d704d379fe6c65b400894f73798bd3f0d9c2c7
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-04-09 11:02:05 -0600

    changed styling of sign up page

--- JSFiles/mainPage.js
+++ JSFiles/mainPage.js
@@ -1,6 +1,9 @@
 $("#login-btn").on("click",(function(e){
-  var div = $('<div id="login"><p id="close" >x</p><h1>Registration</h1><form method="get" action="#" id="sampleForm"><input type="text" placeholder="UserName"/><br><input type="text" name="email" id="email" placeholder="Email" /><br><input type="password" name="pass" id="pass" placeholder="Password" /><br><button id="login-submit" type="submit" >Login</button></form></div>');
-  //idk why but it didn't work when done on seperate lines so i just did it in one :)
+  var div = $('<div id="login"><p id="close" style="width:fit-content;">x</p>'+
+              '<h1>Registration</h1><form method="get" action="#" id="sampleForm"><input type="text" placeholder="Username"/><br>'+
+              '<input type="password" name="pass" id="pass" placeholder="Password" /><br>'+
+              '<button id="login-submit" type="submit" >Login</button></form></div>');
+
   $(this).after(div);
   $("#login").fadeIn(1000);
   //just adds the eventlistener to close the login div/form
