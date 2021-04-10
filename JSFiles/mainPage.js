8ea86c1e51b08f6d90d0f60f84ac372226e00634
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-04-10 17:53:19 -0600

    validate works for login div

--- JSFiles/mainPage.js
+++ JSFiles/mainPage.js
@@ -1,18 +1,35 @@
-$("#login-btn").on("click",(function(e){
-  var div = $('<div id="login"><script type="text/javascript" src="validate.js"></script><p id="close" style="width:fit-content;">x</p>'+
-              '<form method="POST" action="login.php" id="loginform"><input class="required" type="text" placeholder="Username"/><br>'+
-              '<input class="required" type="password" name="pass" id="pass" placeholder="Password" /><br>'+
-              '<a style="font-size:small;padding:5px;" href="html/forgotpassword.html"><u>Forgot Password</u></a><br>'+
-              '<button class="submit" id="login-submit" type="submit" >Login</button></form></div>');
+window.onload = function(){
+    $("#login-btn").on("click",(function(e){
+    var div = $('<div id="login"><p id="close" style="width:fit-content;">x</p>'+
+                '<form method="POST" action="login.php" id="form"><input class="required" type="text" placeholder="Username"/><br>'+
+                '<input class="required" type="password" name="pass" id="pass" placeholder="Password" /><br>'+
+                '<a style="font-size:small;padding:5px;" href="html/forgotpassword.html"><u>Forgot Password</u></a><br>'+
+                '<button class="submit" id="login-submit" type="submit" >Login</button></form></div>');
 
-  $(this).after(div);
-  $("#login").fadeIn(1000);
-  //just adds the eventlistener to close the login div/form
-  $("#close").on("click",(function(e){
-    $("#login").fadeOut(1000);
-    $("#login").remove();
-    console.log("this method is called");
-    // e.preventDefault();
-  
+    $(this).after(div);
+    $("#login").fadeIn(1000);
+    //just adds the eventlistener to close the login div/form
+    $("#form").submit(function (e) { 
+      var inputs = document.getElementsByTagName("input");
+      console.log(inputs);
+      for(var i =0; i<inputs.length;i++){
+        if(inputs[i].value == "" ){
+          e.preventDefault();
+          inputs[i].style.borderWidth = "2px";
+          inputs[i].style.borderColor = "red";
+        }
+        //if the user is typing set the border to white
+          inputs[i].addEventListener("input",function(e){
+            e.target.style.borderColor="white";
+          });
+      }      
+    });
+    $("#close").on("click",(function(e){
+      $("#login").fadeOut(1000);
+      $("#login").remove();
+      console.log("this method is called");
+      // e.preventDefault();
+    
+    }));
   }));
-}));
+}
