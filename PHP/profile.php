8487dd6f23defa4268b33f244debc2c45781fa0d
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-04-14 10:45:07 -0600

    newser.php, profile page

---
+++ PHP/profile.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="eng">
+  <head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewprot" content="width=device-width"> -->
+    <title>Coolblog - Sign up</title>
+    <link rel="stylesheet" href="../css/sign-up.css"/>
+    <link rel="stylesheet" href="../css/main.css"/>
+    <script type="text/javascript" src="../JSFiles/validate.js"></script>
+</head>
+<header>
+  
+    <div class="container">
+        <div class="flexbox align-items-center justify-content-between">
+            <div class="contact-info">
+                <ul class="justify-content-evenly">
+                    <li><p>Contact Info:</p></li>
+                    <li><a><i class="fas fa-phone-square"></i> 250-123-4567</a></li>
+                    <li><a><i class="fas fa-envelope-square"></i> coolblog@cblog.com</a></li>
+                </ul>
+            </div>
+            <div class="page-name">
+                <a class="title" href="#">Cool<span> Blog</span></a>
+            </div>
+            <div class="sns">
+                <ul class="flexbox justify-content-evenly">
+                    <li><a><i class="fab fa-twitter-square"></i></a></li>
+                    <li><a><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a><i class="fab fa-flickr"></i></a></li>
+                    <li><a><i class="fab fa-youtube-square"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <ul class="flexbox justify-content-center justify-content-evenly">
+                <li class="nav-title"><a href="html/nav1.html">Home</a></li>
+                <li class="nav-title"><a href="html/nav2.html">Popular</a></li>
+                <li class="nav-title"><a href="html/nav3.html">Following</a></li>
+                <li class="nav-title"><a href="PHP/profile.php">Profile</a></li>
+                <li class="nav-title" id="login-btn" style="cursor: pointer;">Login</li>
+            </ul>
+        </nav>
+    </div>
+</header>
+  
+<body>
+  <section>
+    <div class="container">
+      <h1>Jill Nim</h1>
+      <h1>Profile Picture: <p><img class="proifle-pic" src="../images/profileIcon.svg"></p></h1>
+      <h1>Email: <h3 style="align-items:center;margin:auto;">jill1@gmail.com</h3></h1>
+    </div>
+  </section>
+</body>
+<footer class="bgDark colorWhite">
+  <div class="container">
+      <div class="g-3-col">
+          <div class="about-us text-align-center">
+              <h3>About Us</h3>
+              <div class="title-container">
+                  <a class="title" href="#">Cool<span> Blog</span></a> 
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque porro magni in molestias quod necessitatibus amet dolorum aliquam, voluptatibus odio rerum laudantium sit eos eveniet labore omnis exercitationem consectetur?</p>
+              <div class="sns">
+                  <ul class="flexbox justify-content-evenly">
+                      <li><a><i class="fab fa-twitter-square"></i></a></li>
+                      <li><a><i class="fab fa-facebook-square"></i></a></li>
+                      <li><a><i class="fab fa-flickr"></i></a></li>
+                      <li><a><i class="fab fa-youtube-square"></i></a></li>
+                  </ul>
+              </div>
+          </div>
+          <div class="tags text-align-center">
+              <h3>Tags</h3>
+              <ul class="flexbox">
+                  <li class="tag"><a>Tag1</a></li>
+                  <li class="tag"><a>Tag2</a></li>
+                  <li class="tag"><a>Tag3</a></li>
+                  <li class="tag"><a>Tag4</a></li>
+                  <li class="tag"><a>Tag5</a></li>
+                  <li class="tag"><a>Tag6</a></li>
+                  <li class="tag"><a>Tag7</a></li>
+              </ul>
+          </div>
+          <div class="tos text-align-center">
+              <h3>Terms of Service</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolor nemo possimus blanditiis, delectus eius rerum fuga ipsa, odit voluptates atque asperiores ratione vero debitis, sit voluptatem neque distinctio labore.</p>
+          </div>
+      </div>
+      <div class="copyright text-align-center">
+          <p>Made by ... &copy;All rights reserved</p>
+      </div>
+  </div>
+</footer>
+<script type="text/javascript" src="validate.js"></script>
+</html>
