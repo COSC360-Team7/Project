a5edbf1bf755c56c6140b8fc8adfff537724dcb9
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-04-16 14:56:35 -0600

    converted blog post to php file

---
+++ PHP/blogpost.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coolblog</title>
+    <link rel="stylesheet" href="../css/main.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/904eedb8ad.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="../JSFiles/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="../JSFiles/validate2.js"></script>
+</head>
+
+<body>
+
+<header>
+    <div class="container">
+        <div class="flexbox align-items-center justify-content-between">
+            <div class="contact-info">
+                <ul class="justify-content-evenly">
+                    <li><p>Contact Info:</p></li>
+                    <li><a><i class="fas fa-phone-square"></i> 250-123-4567</a></li>
+                    <li><a><i class="fas fa-envelope-square"></i> coolblog@cblog.com</a></li>
+                </ul>
+            </div>
+            <div class="page-name">
+                <a class="title" href="../Main.html">Cool<span> Blog</span></a>
+            </div>
+            <div class="sns">
+                <ul class="flexbox justify-content-evenly">
+                    <li><a><i class="fab fa-twitter-square"></i></a></li>
+                    <li><a><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a><i class="fab fa-flickr"></i></a></li>
+                    <li><a><i class="fab fa-youtube-square"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <ul class="flexbox justify-content-center justify-content-evenly">
+                <li class="nav-title"><a href="../Main.html">Home</a></li>
+                <li class="nav-title"><a href="popular.html">Popular</a></li>
+                <li class="nav-title"><a href="following.html">Following</a></li>
+                <li class="nav-title"><a href="../PHP/profile.php">Profile</a></li>
+                <li class="nav-title"><a href="sign-up.html">Sign up</a></li>
+                <li class="nav-title" id="login-btn" style="cursor: pointer;">Login</li>
+                <li class="nav-title"><a href="../PHP/logout.php">Logout</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<section class="blog-banner text-align-center bgBlack colorPrimary">
+    <h1>Blog Title</h1>
+</section>
+
+<section class="blog-detail">
+        <div class="img-container text-align-center">
+        <img class="img-detail" alt="img-alt" src="../images/boat-5889919_1280.png">
+    </div>
+    <div class="blog-text">
+        <h2>Subtitle</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae vero veniam voluptatibus! Placeat culpa, nemo nam neque inventore, voluptas magnam molestias perspiciatis consequatur aliquid, nisi ipsa tempore fugit minima.</p>
+        <p>Voluptas quod recusandae inventore cupiditate, quae molestiae quasi sit eligendi corrupti omnis. Delectus voluptate perferendis dolores? Alias illum eum nulla beatae, minima adipisci laudantium dicta sapiente repellat, non optio corporis!</p>
+        <p>Minima debitis repellat doloremque rem exercitationem veniam numquam magni? Dignissimos consectetur magni tempora officiis minima repellendus optio eum sapiente laboriosam perspiciatis quisquam dolore iure voluptate ducimus, reiciendis voluptates ad fugit.</p>
+        <p>Tenetur officiis ex perspiciatis aspernatur, sunt repellendus, neque alias ad fugit obcaecati dolor ea adipisci sint harum reprehenderit a dolorem repellat fuga facere maiores vel numquam! Cum voluptate cupiditate vero.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae vero veniam voluptatibus! Placeat culpa, nemo nam neque inventore, voluptas magnam molestias perspiciatis consequatur aliquid, nisi ipsa tempore fugit minima.</p>
+        <p>Voluptas quod recusandae inventore cupiditate, quae molestiae quasi sit eligendi corrupti omnis. Delectus voluptate perferendis dolores? Alias illum eum nulla beatae, minima adipisci laudantium dicta sapiente repellat, non optio corporis!</p>
+        <p>Minima debitis repellat doloremque rem exercitationem veniam numquam magni? Dignissimos consectetur magni tempora officiis minima repellendus optio eum sapiente laboriosam perspiciatis quisquam dolore iure voluptate ducimus, reiciendis voluptates ad fugit.</p>
+        <p>Tenetur officiis ex perspiciatis aspernatur, sunt repellendus, neque alias ad fugit obcaecati dolor ea adipisci sint harum reprehenderit a dolorem repellat fuga facere maiores vel numquam! Cum voluptate cupiditate vero.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae vero veniam voluptatibus! Placeat culpa, nemo nam neque inventore, voluptas magnam molestias perspiciatis consequatur aliquid, nisi ipsa tempore fugit minima.</p>
+        <p>Voluptas quod recusandae inventore cupiditate, quae molestiae quasi sit eligendi corrupti omnis. Delectus voluptate perferendis dolores? Alias illum eum nulla beatae, minima adipisci laudantium dicta sapiente repellat, non optio corporis!</p>
+        <p>Minima debitis repellat doloremque rem exercitationem veniam numquam magni? Dignissimos consectetur magni tempora officiis minima repellendus optio eum sapiente laboriosam perspiciatis quisquam dolore iure voluptate ducimus, reiciendis voluptates ad fugit.</p>
+        <p>Tenetur officiis ex perspiciatis aspernatur, sunt repellendus, neque alias ad fugit obcaecati dolor ea adipisci sint harum reprehenderit a dolorem repellat fuga facere maiores vel numquam! Cum voluptate cupiditate vero.</p>
+    </article>
+</div>
+</section>
+
+<footer class="bgDark colorWhite">
+    <div class="container">
+        <div class="g-3-col">
+            <div class="about-us text-align-center">
+                <h3>About Us</h3>
+                <div class="title-container">
+                    <a class="title" href="../Main.html">Cool<span> Blog</span></a> 
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eaque porro magni in molestias quod necessitatibus amet dolorum aliquam, voluptatibus odio rerum laudantium sit eos eveniet labore omnis exercitationem consectetur?</p>
+                <div class="sns">
+                    <ul class="flexbox justify-content-evenly">
+                        <li><a><i class="fab fa-twitter-square"></i></a></li>
+                        <li><a><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a><i class="fab fa-flickr"></i></a></li>
+                        <li><a><i class="fab fa-youtube-square"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="tags text-align-center">
+                <h3>Tags</h3>
+                <ul class="flexbox">
+                    <li class="tag"><a>Tag1</a></li>
+                    <li class="tag"><a>Tag2</a></li>
+                    <li class="tag"><a>Tag3</a></li>
+                    <li class="tag"><a>Tag4</a></li>
+                    <li class="tag"><a>Tag5</a></li>
+                    <li class="tag"><a>Tag6</a></li>
+                    <li class="tag"><a>Tag7</a></li>
+                </ul>
+            </div>
+            <div class="tos text-align-center">
+                <h3>Terms of Service</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolor nemo possimus blanditiis, delectus eius rerum fuga ipsa, odit voluptates atque asperiores ratione vero debitis, sit voluptatem neque distinctio labore.</p>
+            </div>
+        </div>
+        <div class="copyright text-align-center">
+            <p>Made by COSC360 &copy;All rights reserved</p>
+        </div>
+    </div>
+</footer>
+
+
+</body>
+<script type="text/javascript" src="../JSFiles/mainPage.js"></script>
+
+</html>
